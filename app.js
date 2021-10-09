19dcc6774d7e1fad006d189cd5f0763a362dab8d
rijal2 <merentas5benua27@gmail.com>
2021-10-09 12:36:27 +0700

    Membuat file baru menggunakan Node Js, serta menambahkan string ke dalam suatu file

---
+++ app.js
@@ -0,0 +1,5 @@
+// File System (bawaan NODE JS / Core Modules)
+const fs = require('fs');
+
+// Mrnuliskan string ke sebuah file (Sync)
+fs.writeFileSync('test.txt', 'Hello World! Secara synchronus');
