f2f62155561fc698cdf9c841331ac7d480617a87
rijal2 <merentas5benua27@gmail.com>
2021-10-09 22:27:34 +0700

    Membaca isi file menggunakan core module node js, yaitu fs.writeFile

--- app.js
+++ app.js
@@ -2,4 +2,25 @@
 const fs = require('fs');
 
 // Mrnuliskan string ke sebuah file (Sync)
-fs.writeFileSync('test.txt', 'Hello World! Secara synchronus');
+/* fs.writeFileSync('data/test.txt', 'Hello World! Secara synchronus');
+
+// Menuliskan string ke sebuah file (Async)
+/*
+try{
+fs.writeFile('data/test.txt', 'Hello World! Secara Asynchronus gaes Siap.', (e) => console.log(e));
+} catch(e) {
+console.log(e)
+}
+*/
+
+// Membaca file secara Sync
+/*
+const readFile = fs.readFileSync('test.txt', 'utf-8')
+console.log(readFile)
+*/
+
+// Membaca file secara Ashyncrhonus
+fs.readFile('data/test.txt', 'utf-8', (err, data) => {
+if(err) throw err;
+console.log(data)
+})
